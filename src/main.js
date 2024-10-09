27a314f8f880febf3f62d3decc0b46df29852c1c
Nastja Demchyk <NastjaDemchyk@gmail.com>
2024-10-09 23:04:33 +0200

    added izitoast, added all images, created js files and imported them to main.js

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import './js/header.js';
+import './js/hero.js';
+import './js/tech-skills.js';
+import './js/my-projects.js';
+import './js/faq.js';
+import './js/reviews.js';
+import './js/work-together.js';
+import './js/modal-window.js';
+import './js/switcher.js';
