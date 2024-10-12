7a4dbcfab9795b05276ab2226352481bc053e822
Marianna Nishovych <nisovicmarianna@gmail.com>
2024-10-12 12:44:48 +0300

    add mobile/tablet version

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,67 +1,19 @@
-// import Accordion from 'accordion-js';  
-// import 'accordion-js/dist/accordion.min.css';  
+
+import Accordion from 'accordion-js';
+import 'accordion-js/dist/accordion.min.css';
+
+new Accordion('.accordion-container', {
+elementClass: 'ac-item',
+triggerClass: 'faq-icon',
+panelClass: 'ac-panel',
+duration: 800,
+openOnInit: [0],
+beforeOpen: el =>
+    el.querySelector('button.ac-trigger').setAttribute('aria-expanded', true),
+beforeClose: el =>
+    el.querySelector('button.ac-trigger').setAttribute('aria-expanded', false),
+});
 
 
 
-    document.addEventListener('DOMContentLoaded', () => {
-        
-        const faqItems = document.querySelectorAll('li');
-        
 
-        function closeAllElements() {
-            faqItems.forEach(item => {
-                const faqAnswer = item.querySelector('.faq-answer');
-                const faqButton = item.querySelector('.faq-btn');
-                const faqQuestion = item.querySelector('.faq-question');
-
-                faqAnswer.classList.remove('to-reveal');
-                faqButton.classList.remove('active');
-                faqQuestion.classList.remove('active');
-            });
-        }
-
-        faqItems.forEach((item, index) => {
-            const faqAnswer = item.querySelector('.faq-answer');
-            const faqButton = item.querySelector('.faq-btn');
-            const faqQuestion = item.querySelector('.faq-question');
-            
-            faqButton.addEventListener('click', () => {
-                const isActive = faqAnswer.classList.contains('to-reveal');
-
-                if (isActive) {
-                    faqAnswer.classList.remove('to-reveal');
-                    faqButton.classList.remove('active');
-                    faqQuestion.classList.remove('active');
-                } else {
-                    closeAllElements()
-                    faqAnswer.classList.add('to-reveal');
-                    faqButton.classList.add('active');
-                    faqQuestion.classList.add('active');
-
-                    // closeAllElementsAcord(item);
-                }
-            });
-
-            if (index === 0) {
-                faqAnswer.classList.add('to-reveal');
-                faqButton.classList.add('active');
-                // faqQuestion.classList.add('active');
-                // faqAnswer.style.padding = '15px';
-            }
-        });
-
-        // function closeAllElementsAcord(currentItem) {
-        //     faqItems.forEach(item => {
-        //     if (item !== currentItem) {
-        //     const faqAnswer = item.querySelector('.faq-answer');
-        //     const faqButton = item.querySelector('.faq-btn');
-        //     const faqQuestion = item.querySelector('.faq-question');
-            
-        //     faqAnswer.classList.remove('to-reveal');
-        //     faqButton.classList.remove('active');
-        //     faqQuestion.classList.remove('active');
-        //     }
-        // });
-        // }
-
-    })
