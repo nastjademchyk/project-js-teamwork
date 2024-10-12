b7a6eacfd8ae75072afba177fffe9a01289ee898
Sergey-2410 <167428697+Sergey-2410@users.noreply.github.com>
2024-10-12 16:38:02 +0300

    update my-projects

--- src/js/my-projects.js
+++ src/js/my-projects.js
@@ -1,1 +1,45 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+const projects = document.querySelectorAll('.project-item');
+const loadMoreBtn = document.querySelector('.btn-load');
+let currentIndex = 0;
 
+const showItems = () => {
+  projects.forEach((item, index) => {
+    if (index >= currentIndex && index < currentIndex + 3) {
+      item.classList.remove('is-hidden');
+      loadMoreBtn.classList.remove('is-hidden');
+    }
+  });
+
+  if (scrollHeight > 0) {
+    window.scrollBy({
+      top: scrollHeight,
+      left: 0,
+      behavior: 'smooth',
+    });
+  }
+  currentIndex += 3;
+  if (currentIndex >= projects.length) {
+    loadMoreBtn.classList.add('is-hidden');
+    iziToast.info({
+      message: 'Sorry, this is the last project',
+      position: 'topRight',
+    });
+  }
+};
+
+showItems();
+loadMoreBtn.addEventListener('click', showItems);
+
+// function loadMoreScroll() {
+//   const lastHit = gallery.lastElementChild;
+//   const hitHeight = lastHit.getBoundingClientRect().height;
+//   const scrollHeight = hitHeight * 3;
+
+//   window.scrollBy({
+//     top: scrollHeight,
+//     left: 0,
+//     behavior: 'smooth',
+//   });
+// }
