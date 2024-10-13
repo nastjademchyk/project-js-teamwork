7bf07676cb020b81abcbaaa7cab9f41cda58fee9
Nastja Demchyk <NastjaDemchyk@gmail.com>
2024-10-13 18:22:35 +0200

    added js to modal window

--- src/js/modal-window.js
+++ src/js/modal-window.js
@@ -1,1 +1,20 @@
+const modalWindow = document.querySelector('.backdrop');
+const closeModalButton = document.querySelector('.modal-close-btn');
 
+function closeModalWindow() {
+  modalWindow.style.display = 'none';
+}
+
+closeModalButton.addEventListener('click', closeModalWindow);
+
+document.addEventListener('keydown', function (event) {
+  if (event.key === 'Escape') {
+    closeModalWindow();
+  }
+});
+
+modalWindow.addEventListener('click', function (event) {
+  if (event.target === modalWindow) {
+    closeModalWindow();
+  }
+});
