3de2a72cbb20dff009353aceda1927f96136b0de
Irina-Piskokha <4526937@gmail.com>
2024-10-13 17:42:08 +0300

    add js

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,19 +1,14 @@
-
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 
 new Accordion('.accordion-container', {
-elementClass: 'ac-item',
-triggerClass: 'ac-header',
-panelClass: 'ac-panel',
-duration: 600,
-openOnInit: [0],
-beforeOpen: el =>
+  elementClass: 'ac-item',
+  triggerClass: 'ac-header',
+  panelClass: 'ac-panel',
+  duration: 3000,
+  openOnInit: [0],
+  beforeOpen: el =>
     el.querySelector('button.ac-trigger').setAttribute('aria-expanded', true),
-beforeClose: el =>
+  beforeClose: el =>
     el.querySelector('button.ac-trigger').setAttribute('aria-expanded', false),
 });
-
-
-
-
--- src/js/work-together.js
+++ src/js/work-together.js
@@ -7,8 +7,8 @@ const refs = {
   comments: document.querySelector('.form-input-comments'),
 };
 
-const BASE_URL = 'https://portfolio-js.b.goit.study/api';
-const ENDPOINT = 'requests';
+const BASE_URL = 'https://portfolio-js.b.goit.study/api/requests';
+// const ENDPOINT = 'requests';
 
 refs.form.addEventListener('submit', onFormSubmit);
 
@@ -21,6 +21,8 @@ async function onFormSubmit(event) {
     email: email.value,
     comments: comments.value,
   };
+
+  console.log(objData);
 
   //   =================================================================
   //   перевіряємо чи порожні поля вводу на формі
@@ -46,34 +48,123 @@ async function onFormSubmit(event) {
     return;
   }
 
+  //    ===================================================================
+
   //   =================================================================
   // відправляємо форму на сервер
+  //   try {
+  //     const response = await fetch(`${BASE_URL}`, {
+  //       method: 'POST',
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //       },
+  //       body: JSON.stringify(objData),
+  //     });
+
+  //     console.log(objData);
+  //     console.log(response);
+
+  //     if (response.ok) {
+  //       console.log('ok');
+  //     }
+
+  //     if (!response.ok) {
+  //       iziToast.error({
+  //         title: 'Sorry!',
+  //         message: 'An error occurred, please try againe',
+  //         position: 'center',
+  //         messageColor: '#E74A3B',
+  //         closeOnEscape: true,
+  //         icon: '',
+  //       });
+  //     }
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+
+  //   ====================================================================
+
+  //   const options = {
+  //     method: 'POST',
+  //     body: JSON.stringify(objData),
+  //     headers: {
+  //       'Content-Type': 'application/json; charset=UTF-8',
+  //     },
+  //   };
+
+  //   fetch('https://portfolio-js.b.goit.study/api/requests', options)
+  //     .then(response => {
+  //       if (!response.ok) {
+  //         throw new Error(response.status);
+  //       }
+  //       return response.json();
+  //     })
+  //     .then(post => console.log(post))
+  //     .catch(error => console.log(error));
+}
+
+// ========================================================================
+// function addForm(objData) {
+//   return fetch(`${BASE_URL}`, {
+//     method: 'POST',
+//     body: JSON.stringify(objData),
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//   }).then(checkResponse);
+// }
+
+// addForm();
+
+//  ====================================================================
+
+const options = {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    accept: 'application/json',
+  },
+};
+const createContact = async post => {
+  options.body = JSON.stringify(post);
   try {
-    const response = await fetch(`${BASE_URL}/${ENDPOINT}`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(objData),
-    });
-
-    console.log(response);
-
-    if (response.ok) {
-      console.log('ok');
-    }
-
-    if (!response.ok) {
-      iziToast.error({
-        title: 'Sorry!',
-        message: 'An error occurred, please try againe',
-        position: 'center',
-        messageColor: '#E74A3B',
-        closeOnEscape: true,
-        icon: '',
-      });
-    }
+    const response = await fetch(
+      'https:portfolio-js.b.goit.study/api/requests',
+      options
+    );
+    if (!response.ok) throw new Error(response.status);
+    return response.json();
   } catch (error) {
     console.log(error);
+    throw error;
   }
-}
+};
+
+const onSbmit = e => {
+  e.preventDefault();
+
+  //   const {
+  //     email: { value: email },
+  //     comment: { value: comment },
+  //   } = e.target.elements;
+
+  const { email, comments } = e.target.elements;
+
+  const objData = {
+    email: email.value,
+    comments: comments.value,
+  };
+
+  createContact(objData)
+    .then(({ title, message }) => {
+      e.target.reset();
+    })
+    .catch(error => {
+      errorMessege(
+        ` Please verify the information you provided and try again `
+      );
+    });
+};
+refs.form.addEventListener('submit', onSbmit);
+
+// ===============================================================
