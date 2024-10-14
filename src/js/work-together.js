6f0159ec35c489b5983f09150b8e3c28197b1201
Irina-Piskokha <4526937@gmail.com>
2024-10-14 09:45:19 +0300

    add end js form

--- src/js/work-together.js
+++ src/js/work-together.js
@@ -10,114 +10,6 @@ const refs = {
 const BASE_URL = 'https://portfolio-js.b.goit.study/api/requests';
 // const ENDPOINT = 'requests';
 
-refs.form.addEventListener('submit', onFormSubmit);
-
-async function onFormSubmit(event) {
-  event.preventDefault();
-
-  const { email, comments } = event.target.elements;
-
-  const objData = {
-    email: email.value,
-    comments: comments.value,
-  };
-
-  console.log(objData);
-
-  //   =================================================================
-  //   перевіряємо чи порожні поля вводу на формі
-  if (objData.email.trim() === '') {
-    iziToast.error({
-      message: 'Field email is empty',
-      position: 'center',
-      messageColor: '#E74A3B',
-      closeOnEscape: true,
-      icon: '',
-    });
-    return;
-  }
-
-  if (objData.comments.trim() === '') {
-    iziToast.error({
-      message: 'Field comments is empty',
-      position: 'center',
-      messageColor: '#E74A3B',
-      closeOnEscape: true,
-      icon: '',
-    });
-    return;
-  }
-
-  //    ===================================================================
-
-  //   =================================================================
-  // відправляємо форму на сервер
-  //   try {
-  //     const response = await fetch(`${BASE_URL}`, {
-  //       method: 'POST',
-  //       headers: {
-  //         'Content-Type': 'application/json',
-  //       },
-  //       body: JSON.stringify(objData),
-  //     });
-
-  //     console.log(objData);
-  //     console.log(response);
-
-  //     if (response.ok) {
-  //       console.log('ok');
-  //     }
-
-  //     if (!response.ok) {
-  //       iziToast.error({
-  //         title: 'Sorry!',
-  //         message: 'An error occurred, please try againe',
-  //         position: 'center',
-  //         messageColor: '#E74A3B',
-  //         closeOnEscape: true,
-  //         icon: '',
-  //       });
-  //     }
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-
-  //   ====================================================================
-
-  //   const options = {
-  //     method: 'POST',
-  //     body: JSON.stringify(objData),
-  //     headers: {
-  //       'Content-Type': 'application/json; charset=UTF-8',
-  //     },
-  //   };
-
-  //   fetch('https://portfolio-js.b.goit.study/api/requests', options)
-  //     .then(response => {
-  //       if (!response.ok) {
-  //         throw new Error(response.status);
-  //       }
-  //       return response.json();
-  //     })
-  //     .then(post => console.log(post))
-  //     .catch(error => console.log(error));
-}
-
-// ========================================================================
-// function addForm(objData) {
-//   return fetch(`${BASE_URL}`, {
-//     method: 'POST',
-//     body: JSON.stringify(objData),
-//     headers: {
-//       'Content-Type': 'application/json',
-//     },
-//   }).then(checkResponse);
-// }
-
-// addForm();
-
-//  ====================================================================
-
 const options = {
   method: 'POST',
   headers: {
@@ -128,10 +20,7 @@ const options = {
 const createContact = async post => {
   options.body = JSON.stringify(post);
   try {
-    const response = await fetch(
-      'https:portfolio-js.b.goit.study/api/requests',
-      options
-    );
+    const response = await fetch(`${BASE_URL}`, options);
     if (!response.ok) throw new Error(response.status);
     return response.json();
   } catch (error) {
@@ -143,21 +32,18 @@ const createContact = async post => {
 const onSbmit = e => {
   e.preventDefault();
 
-  //   const {
-  //     email: { value: email },
-  //     comment: { value: comment },
-  //   } = e.target.elements;
-
   const { email, comments } = e.target.elements;
 
   const objData = {
     email: email.value,
-    comments: comments.value,
+    comment: comments.value,
   };
 
   createContact(objData)
     .then(({ title, message }) => {
       e.target.reset();
+      console.log(title);
+      console.log(message);
     })
     .catch(error => {
       errorMessege(
