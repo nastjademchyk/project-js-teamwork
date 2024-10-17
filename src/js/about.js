de6094777ac7f469d1ab55c5e0d89771ea6b2b57
Nastja Demchyk <NastjaDemchyk@gmail.com>
2024-10-17 19:21:53 +0200

    added dark theme for sticky header

--- src/js/about.js
+++ src/js/about.js
@@ -1,46 +1,45 @@
-// import anime from 'animejs/lib/anime.es.js';
+import anime from 'animejs/lib/anime.es.js';
 
-// document.addEventListener("DOMContentLoaded", () => {
-//   const aboutImage = document.querySelector('.about-image');
+document.addEventListener('DOMContentLoaded', () => {
+  const aboutImage = document.querySelector('.about-image');
 
-//   aboutImage.addEventListener('mouseover', () => {
-//     anime({
-//       targets: '.about-image',
-//       rotate: '1turn',
-//       scale: [1, 1.1],
-//       duration: 1000,
-//       easing: 'easeInOutQuad'
-//     });
+  aboutImage.addEventListener('mouseover', () => {
+    anime({
+      targets: '.about-image',
+      rotate: '1turn',
+      scale: [1, 1.1],
+      duration: 1000,
+      easing: 'easeInOutQuad',
+    });
+  });
 
-//   });
+  aboutImage.addEventListener('mouseout', () => {
+    anime({
+      targets: '.about-image',
+      rotate: '0turn',
+      scale: [1.1, 1],
+      duration: 1000,
+      easing: 'easeInOutQuad',
+    });
+  });
 
-//   aboutImage.addEventListener('mouseout', () => {
-//     anime({
-//       targets: '.about-image',
-//       rotate: '0turn',
-//       scale: [1.1, 1],
-//       duration: 1000,
-//       easing: 'easeInOutQuad'
-//     });
-//   });
+  aboutImage.addEventListener('focus', () => {
+    anime({
+      targets: '.about-image',
+      rotate: '1turn',
+      scale: [1, 1.1],
+      duration: 1000,
+      easing: 'easeInOutQuad',
+    });
+  });
 
-//   aboutImage.addEventListener('focus', () => {
-//     anime({
-//       targets: '.about-image',
-//       rotate: '1turn',
-//       scale: [1, 1.1],
-//       duration: 1000,
-//       easing: 'easeInOutQuad'
-//     });
-//   });
-
-//   aboutImage.addEventListener('blur', () => {
-//     anime({
-//       targets: '.about-image',
-//       rotate: '0turn',
-//       scale: [1.1, 1],
-//       duration: 1000,
-//       easing: 'easeInOutQuad'
-//     });
-//   });
-// });
+  aboutImage.addEventListener('blur', () => {
+    anime({
+      targets: '.about-image',
+      rotate: '0turn',
+      scale: [1.1, 1],
+      duration: 1000,
+      easing: 'easeInOutQuad',
+    });
+  });
+});
